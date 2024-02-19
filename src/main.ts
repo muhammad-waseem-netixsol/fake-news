d3051d08de87439058ad1f88afcc85f688cd3cde
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-02-19 17:40:22 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: "http://localhost:3000"})
+  // Define CORS options
+  const corsOptions = {
+    origin: ['https://finalhackathonfrontend.vercel.app'], // or specify your frontend URL(s) here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // or specify the methods you need
+    allowedHeaders: ['Content-Type', 'Authorization'], // or specify the headers you need
+  };
+  // Enable CORS with options
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
