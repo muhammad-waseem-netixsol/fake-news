b27fa7a0596711c04aba85498a149cfd241085be
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-02-19 17:36:54 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors: false});
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: "http://localhost:3000"})
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
